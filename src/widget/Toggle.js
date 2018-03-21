b166ac033596c9b449af0ea7e9532ec339334f87
bencode <bencode@163.com>
2018-03-21 22:20:44 +0800

    完成toggle

--- src/widget/Toggle.js
+++ src/widget/Toggle.js
@@ -1,18 +1,17 @@
 import delegate from 'delegate';
 
-
 export default function(el, options = {}) {
   const event = options.event || 'click';
   const className = options.className || 'active';
   const selector = options.selector;
-  const toggle = elm => elm.classList.toggle(className);
+  const toggle = current => {
+    const elm = options.target ? document.querySelector(options.target) : current;
+    elm.classList.toggle(className);
+  };
+
   if (selector) {
-    delegate(el, selector, event, e => {
-      toggle(e.delegateTarget, className);
-    });
+    delegate(el, selector, event, e => toggle(e.delegateTarget));
   } else {
-    el.addEventListener(event, () => {
-      toggle(el, className);
-    });
+    el.addEventListener(event, () => toggle(el));
   }
 }
